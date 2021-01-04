476854115323ca18aa7927e39c759beb3a2bc4a1
AdamG95 <45023561+AdamG95@users.noreply.github.com>
2021-01-03 20:03:20 -0600

    basic layout for addMedication created

--- app/src/main/java/projects/medicationtracker/addMedication.java
+++ app/src/main/java/projects/medicationtracker/addMedication.java
@@ -1,11 +1,14 @@
 package projects.medicationtracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -13,6 +16,8 @@ import java.util.ArrayList;
 public class addMedication extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
     Spinner timeSpinner;
+    EditText takenEvery;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,12 +26,13 @@ public class addMedication extends AppCompatActivity implements AdapterView.OnIt
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         timeSpinner = findViewById(R.id.timeSpinner);
+        takenEvery = findViewById(R.id.medFrequencyEnter);
 
         ArrayList<String> timeUnits = new ArrayList<>();
-        timeUnits.add("Hour");
-        timeUnits.add("Day");
-        timeUnits.add("Week");
-        timeUnits.add("Month");
+        timeUnits.add("Hour(s)");
+        timeUnits.add("Day(s)");
+        timeUnits.add("Week(s)");
+        timeUnits.add("Month(s)");
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, timeUnits);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         timeSpinner.setAdapter(arrayAdapter);
@@ -36,14 +42,19 @@ public class addMedication extends AppCompatActivity implements AdapterView.OnIt
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
     {
-
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView)
     {
-
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId() == android.R.id.home)
+            finish();
 
+        return super.onOptionsItemSelected(item);
+    }
 }
