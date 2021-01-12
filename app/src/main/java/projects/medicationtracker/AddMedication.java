394424d9236c67d620ea8f693b8743622ac4ab4a
AdamG95 <45023561+AdamG95@users.noreply.github.com>
2021-01-11 22:28:06 -0600

    Improvements made to database and AddMedication

--- app/src/main/java/projects/medicationtracker/AddMedication.java
+++ app/src/main/java/projects/medicationtracker/AddMedication.java
@@ -5,16 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.annotation.SuppressLint;
+import android.database.DatabaseUtils;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
@@ -25,13 +25,15 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.sql.Time;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
 
-public class AddMedication<volitile> extends AppCompatActivity
+public class AddMedication extends AppCompatActivity
 {
     RadioGroup patientGroup;
     RadioGroup frequencyGroup;
@@ -43,6 +45,7 @@ public class AddMedication<volitile> extends AppCompatActivity
     TextView hiddenTextView;
     Spinner frequencySpinner;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -71,22 +74,17 @@ public class AddMedication<volitile> extends AppCompatActivity
         AutoCompleteTextView nameInput = findViewById(R.id.patientNameNotMe);
 
 
-        patientGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        patientGroup.setOnCheckedChangeListener((radioGroup, i) ->
         {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i)
+            switch (radioGroup.findViewById(i).getId())
             {
-                switch (radioGroup.findViewById(i).getId())
-                {
-                    case R.id.meButton:
-                        if (nameInput.getVisibility() == View.VISIBLE)
-                            nameInput.setVisibility(View.GONE);
-                        break;
-                    case R.id.otherButton:
-                        nameInput.setVisibility(View.VISIBLE);
-                        break;
-                }
+                case R.id.meButton:
+                    if (nameInput.getVisibility() == View.VISIBLE)
+                        nameInput.setVisibility(View.GONE);
+                    break;
+                case R.id.otherButton:
+                    nameInput.setVisibility(View.VISIBLE);
+                    break;
             }
         });
 
@@ -111,90 +109,69 @@ public class AddMedication<volitile> extends AppCompatActivity
                         textView.setInputType(InputType.TYPE_CLASS_DATETIME | InputType.TYPE_DATETIME_VARIATION_TIME);
                         // set layout params
                         timesOfTheDay.addView(textView);
-                        textView.setOnClickListener(new View.OnClickListener()
+                        textView.setOnClickListener(view ->
                         {
-                            @Override
-                            public void onClick(View view)
-                            {
-                                DialogFragment dialogFragment = new TimePickerFragment();
-                                dialogFragment.show(getSupportFragmentManager(), null);
-                                textView.setTag(hiddenTextView.getTag());
-                                textView.setText(hiddenTextView.getText());
-                            }
+                            DialogFragment dialogFragment = new TimePickerFragment();
+                            dialogFragment.show(getSupportFragmentManager(), null);
+                            textView.setTag(hiddenTextView.getTag());
+                            textView.setText(hiddenTextView.getText());
                         });
                     }
                 }
             }
         });
 
-        frequencyGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        frequencyGroup.setOnCheckedChangeListener((radioGroup, i) ->
         {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i)
+            TextView timeTaken1 = findViewById(R.id.timeTaken1);
+            switch (radioGroup.findViewById(i).getId())
             {
-                TextView timeTaken1 = findViewById(R.id.timeTaken1);
-                switch (radioGroup.findViewById(i).getId())
-                {
-                    case R.id.multplePerDayButton:
-                        timeTaken1.setVisibility(View.GONE);
-                        customFrequencyLayout.setVisibility(View.GONE);
-                        numTimesTaken.setVisibility(View.VISIBLE);
-                        numTimesTaken.setText("");
-                        break;
-                    case R.id.dailyButton:
-                        numTimesTaken.setVisibility(View.GONE);
-                        customFrequencyLayout.setVisibility(View.GONE);
-                        timesOfTheDay.removeAllViews();
-                        timeTaken1.setVisibility(View.VISIBLE);
-                        timeTaken1.setText(R.string.atThisTime);
-                        timeTaken1.setOnClickListener(new View.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(View view)
-                            {
-                                DialogFragment dialogFragment = new TimePickerFragment();
-                                dialogFragment.show(getSupportFragmentManager(), null);
-                                timeTaken1.setTag(hiddenTextView.getTag());
-                                timeTaken1.setText(hiddenTextView.getText().toString());
-                            }
-                        });
-                        break;
-                    case R.id.customFreqButton:
-                        timeTaken1.setVisibility(View.GONE);
-                        numTimesTaken.setVisibility(View.GONE);
-                        timesOfTheDay.removeAllViews();
-                        customFrequencyLayout.setVisibility(View.VISIBLE);
+                case R.id.multplePerDayButton:
+                    timeTaken1.setVisibility(View.GONE);
+                    customFrequencyLayout.setVisibility(View.GONE);
+                    numTimesTaken.setVisibility(View.VISIBLE);
+                    numTimesTaken.setText("");
+                    break;
+                case R.id.dailyButton:
+                    numTimesTaken.setVisibility(View.GONE);
+                    customFrequencyLayout.setVisibility(View.GONE);
+                    timesOfTheDay.removeAllViews();
+                    timeTaken1.setVisibility(View.VISIBLE);
+                    timeTaken1.setText(R.string.atThisTime);
+                    timeTaken1.setOnClickListener(view ->
+                    {
+                        DialogFragment dialogFragment = new TimePickerFragment();
+                        dialogFragment.show(getSupportFragmentManager(), null);
+                        timeTaken1.setTag(hiddenTextView.getTag());
+                        timeTaken1.setText(hiddenTextView.getText().toString());
+                    });
+                    break;
+                case R.id.customFreqButton:
+                    timeTaken1.setVisibility(View.GONE);
+                    numTimesTaken.setVisibility(View.GONE);
+                    timesOfTheDay.removeAllViews();
+                    customFrequencyLayout.setVisibility(View.VISIBLE);
 
-                        TextView[] textViews = new TextView[2];
-                        textViews[0] = findViewById(R.id.startDate);
-                        textViews[1] = findViewById(R.id.startTime);
+                    TextView[] textViews = new TextView[2];
+                    textViews[0] = findViewById(R.id.startDate);
+                    textViews[1] = findViewById(R.id.startTime);
 
-                        getCurrentTimeAndDate(textViews[0], textViews[1]);
+                    getCurrentTimeAndDate(textViews[0], textViews[1]);
 
-                        textViews[0].setOnClickListener(new View.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(View view)
-                            {
-                                DialogFragment dialogFragment = new SelectDateFragment();
-                                dialogFragment.show(getSupportFragmentManager(), null);
-                            }
-                        });
+                    textViews[0].setOnClickListener(view ->
+                    {
+                        DialogFragment dialogFragment = new SelectDateFragment();
+                        dialogFragment.show(getSupportFragmentManager(), null);
+                    });
 
-                        textViews[1].setOnClickListener(new View.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(View view)
-                            {
-                                DialogFragment dialogFragment = new TimePickerFragment();
-                                dialogFragment.show(getSupportFragmentManager(), null);
-                                textViews[1].setTag(hiddenTextView.getTag());
-                                textViews[1].setText(hiddenTextView.getText().toString());
-                            }
-                        });
-                        break;
-                }
+                    textViews[1].setOnClickListener(view ->
+                    {
+                        DialogFragment dialogFragment = new TimePickerFragment();
+                        dialogFragment.show(getSupportFragmentManager(), null);
+                        textViews[1].setTag(hiddenTextView.getTag());
+                        textViews[1].setText(hiddenTextView.getText().toString());
+                    });
+                    break;
             }
         });
     }
@@ -220,9 +197,107 @@ public class AddMedication<volitile> extends AppCompatActivity
         // Check that all fields are fill and contain valid values
         if (!allFieldsFilled())
             return;
+
         // Prepare data for submission to database
+        RadioButton meButton = findViewById(R.id.meButton);
+        RadioButton multiplePerDay = findViewById(R.id.multplePerDayButton);
+        RadioButton dailyButton = findViewById(R.id.dailyButton);
+        EditText patientNameNotMe = findViewById(R.id.patientNameNotMe);
+        EditText medicationNameEntry = findViewById(R.id.medNameEnter);
+        EditText medicationDosage = findViewById(R.id.medDosageEnter);
+        EditText medicationUnits = findViewById(R.id.editTextUnits);
+        EditText takenEvery = findViewById(R.id.enterFrequency);
+        TextView startDate = findViewById(R.id.startDate);
+        TextView startTime = findViewById(R.id.startTime);
+        int[] hourMin = (int[]) startTime.getTag();
+        ArrayList<String> times = new ArrayList<>();
+        String patient;
+        String medName;
+        String dosage;
+        String medUnits;
+        float timesPerFrequency = Float.parseFloat(takenEvery.getText().toString());
+
+        if (meButton.isChecked())
+            patient = meButton.getTag().toString();
+        else
+            patient = patientNameNotMe.getText().toString();
+
+        medName = medicationNameEntry.getText().toString();
+        dosage = medicationDosage.getText().toString();
+        medUnits = medicationUnits.getText().toString();
+
+        if (multiplePerDay.isChecked())
+        {
+            LinearLayout timesOfDay = findViewById(R.id.timesOfTheDay);
+            TextView textView;
+
+            for (int i = 0; i < timesOfDay.getChildCount(); i++)
+            {
+                textView = findViewById(i);
+                int[] timeArr = (int[]) textView.getTag();
+                times.add(formatTime(timeArr[0], timeArr[1]));
+            }
+        }
+        else if (dailyButton.isChecked())
+        {
+            TextView timeTaken = findViewById(R.id.timeTaken1);
+            int[] timeArr = (int[]) timeTaken.getTag();
+            times.add(formatTime(timeArr[0], timeArr[1]));
+        }
+        else
+        {
+            Spinner frequencySpinner = findViewById(R.id.frequencySpinner);
+            Adapter adapter = frequencySpinner.getAdapter();
+            ArrayList<String> spinnerOptions = new ArrayList<>();
+            String frequency = frequencySpinner.getSelectedItem().toString();
+            String startDatetime = startDate.getText().toString() + " " + formatTime(hourMin[0], hourMin[1]);
+            ArrayList<Date> dates = new ArrayList<>();
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date firstDose;
+
+            try
+            {
+                firstDose = simpleDateFormat.parse(startDatetime);
+            }
+            catch (ParseException e)
+            {
+                e.printStackTrace();
+                Toast.makeText(this, "An error occurred when formatting date", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            dates.add(firstDose);
+
+            for (int i = 0; i < adapter.getCount(); i++)
+                spinnerOptions.add((String) adapter.getItem(i));
+
+            if (frequency.equals(spinnerOptions.get(0)))
+            {
+
+            }
+            else if (frequency.equals(spinnerOptions.get(1)))
+            {
+
+            }
+            else
+            {
+
+            }
+
+        }
 
         // Submit to database, return to MainActivity
+        DBHelper helper = new DBHelper(this);
+        boolean val = helper.addMedication(medName, patient, dosage, medUnits, times);
+
+        if (!val)
+        {
+            Toast.makeText(this, "An error Occurred", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        finish();
     }
 
 
@@ -245,32 +320,68 @@ public class AddMedication<volitile> extends AppCompatActivity
         time.setText(dateForUser[1]);
     }
 
+    public String formatTime (int hours, int minutes)
+    {
+        String dateTime;
+        Date today = new Date();
+        LocalDate localDate = today.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int year = localDate.getYear();
+        int month = localDate.getMonthValue();
+        int day = localDate.getDayOfMonth();
+
+        dateTime = year + "/" + month + "/" + day + " ";
+
+        if (hours < 10)
+            dateTime += "0" + hours + ":";
+        else
+            dateTime += hours + ":";
+
+        if (minutes < 10)
+            dateTime += "0" + minutes + ":00";
+        else
+            dateTime += minutes + ":00";
+
+        return dateTime;
+    }
+
     public boolean allFieldsFilled()
     {
         int trueCount = 0;
         RadioButton[] patientButtons = new RadioButton[2];
         RadioButton[] frequencyButtons = new RadioButton[3];
         EditText nameEntry = findViewById(R.id.patientNameNotMe);
+        EditText medName = findViewById(R.id.medNameEnter);
         EditText dosage = findViewById(R.id.medDosageEnter);
         EditText units = findViewById(R.id.editTextUnits);
 
         patientButtons[0] = findViewById(R.id.meButton);
         patientButtons[1] = findViewById(R.id.otherButton);
 
-        if (patientButtons[0].isSelected())
+        if (patientButtons[0].isChecked())
             trueCount++;
-        else if (patientButtons[1].isSelected())
+        else if (patientButtons[1].isChecked())
         {
             if (TextUtils.isEmpty(nameEntry.getText().toString()))
                 nameEntry.setError("Please enter a name");
-            else if (!nameEntry.getText().toString().equals("ME!"))
+            else if (nameEntry.getText().toString().equals("ME!"))
                 nameEntry.setError("Name cannot be \"ME!\"");
             else
                 trueCount++;
         }
+        else
+            Toast.makeText(this, "Please choose a patient option", Toast.LENGTH_SHORT).show();
 
-        if (TextUtils.isEmpty(dosage.getText().toString()) && Integer.parseInt(dosage.getText().toString()) > 0)
+        if (TextUtils.isEmpty(medName.getText().toString()))
+            medName.setError("Please enter the medication's name");
+        else
+            trueCount++;
+
+        if (TextUtils.isEmpty(dosage.getText().toString()))
             dosage.setError("Please enter a dosage");
+        else if (Integer.parseInt(dosage.getText().toString()) <= 0)
+        {
+            dosage.setError("Enter a number greater than 0");
+        }
         else
             trueCount++;
 
@@ -282,21 +393,33 @@ public class AddMedication<volitile> extends AppCompatActivity
         frequencyButtons[0] = findViewById(R.id.multplePerDayButton);
         frequencyButtons[1] = findViewById(R.id.dailyButton);
         frequencyButtons[2] = findViewById(R.id.customFreqButton);
+        EditText takenEvery = findViewById(R.id.enterFrequency);
+        TextView timeTaken1 = findViewById(R.id.timeTaken1);
 
-        if (frequencyButtons[0].isSelected())
+        if (frequencyButtons[0].isChecked())
         {
             EditText timesPerDay = findViewById(R.id.numTimesTaken);
-            if (TextUtils.isEmpty(timesPerDay.toString()))
+            if (TextUtils.isEmpty(timesPerDay.getText().toString()))
                 timesPerDay.setError("Please enter the number of times per day this medication is taken");
+            else
+                trueCount++;
         }
-        else if (frequencyButtons[1].isSelected())
+        else if (frequencyButtons[1].isChecked())
         {
-
+            if (timeTaken1.getText().toString().equals(getString(R.string.atThisTime)))
+                Toast.makeText(this, "Please enter a time", Toast.LENGTH_SHORT).show();
+            else
+                trueCount++;
         }
-        else if (frequencyButtons[2].isSelected())
+        else if (frequencyButtons[2].isChecked())
         {
-
+            if (TextUtils.isEmpty(takenEvery.getText().toString()))
+                takenEvery.setError("Please enter a number");
+            else
+                trueCount++;
         }
+        else
+            Toast.makeText(this, "Please choose a frequency option", Toast.LENGTH_SHORT).show();
 
         return trueCount == 5;
     }
--- app/src/main/java/projects/medicationtracker/DBHelper.java
+++ app/src/main/java/projects/medicationtracker/DBHelper.java
@@ -1,9 +1,11 @@
 package projects.medicationtracker;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -12,36 +14,33 @@ import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper
 {
-    public final int NUM_TABLES = 5;
+    private final int NUM_TABLES = 4;
 
-    public static final String DATABASE_NAME = "Medications.db";
+    private static final String DATABASE_NAME = "Medications.db";
 
-    public static final String MEDICATION_TABLE = "Medication";
-    public static final String MED_ID = "MedicationID";
-    public static final String MED_NAME = "MedName";
-    public static final String PATIENT_NAME = "PatientName";
-    public static final String MED_FREQUENCY = "DoseFrequency";
-    public static final String MED_DOSAGE = "Dosage";
-    public static final String MED_UNITS = "Units";
+    private static final String MEDICATION_TABLE = "Medication";
+    private static final String MED_ID = "MedicationID";
+    private static final String MED_NAME = "MedName";
+    private static final String PATIENT_NAME = "PatientName";
+    private static final String MED_DOSAGE = "Dosage";
+    private static final String MED_UNITS = "Units";
 
-    public static final String MEDICATION_TRACKER_TABLE = "MedicationTracker";
-    public static final String LAST_TAKEN = "LastTaken";
-    public static final String NEXT_DOSE = "NextDose";
+    private static final String MEDICATION_TRACKER_TABLE = "MedicationTracker";
+    private static final String DOSE_TIME = "DoseTime";
+    private static final String TAKEN = "Taken";
+    private static final String TIME_TAKEN = "TimeTaken";
+    private static final String DOSE_ID = "DoseID";
 
-    public static final String MEDICATION_HISTORY = "MedicationHistory";
-    public static final String TAKEN = "Taken";
-    public static final String TIME_TAKEN = "DATETIME";
+    private static final String MEDICATION_STATS_TABLE = "MedicationStats";
+    private static final String START_DATE = "StartDate";
+    private static final String END_DATE = "EndDate";
+    private static final String DOSES_TAKEN = "DosesTaken";
+    private static final String DOSES_MISSED = "DosesMissed";
 
-    public static final String MEDICATION_STATS_TABLE = "MedicationStats";
-    public static final String START_DATE = "StartDate";
-    public static final String END_DATE = "EndDate";
-    public static final String DOSES_TAKEN = "DosesTaken";
-    public static final String DOSES_MISSED = "DosesMissed";
-
-    public static final String NOTES_TABLE = "Notes";
-    public static final String NOTE_ID = "NoteID";
-    public static final String NOTE = "Note";
-    public static final String ENTRY_TIME = "EntryTime";
+    private static final String NOTES_TABLE = "Notes";
+    private static final String NOTE_ID = "NoteID";
+    private static final String NOTE = "Note";
+    private static final String ENTRY_TIME = "EntryTime";
 
 
     public DBHelper(@Nullable Context context)
@@ -55,52 +54,48 @@ public class DBHelper extends SQLiteOpenHelper
         String[] queries = new String[NUM_TABLES];
 
         queries[0] = "CREATE TABLE " + MEDICATION_TABLE + "("
-                + MED_ID + " INT, "
+                + MED_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + MED_NAME + " TEXT,"
                 + PATIENT_NAME + " Text,"
-                + MED_FREQUENCY + " INT, "
-                + MED_DOSAGE + " DECIMAL(3,2), "
-                + MED_UNITS + " TEXT,"
-                + "PRIMARY KEY (" + MED_ID + ")"
+                + MED_DOSAGE + " DECIMAL(3,2),"
+                + MED_UNITS + " TEXT"
                 + ")";
 
         queries[1] = "CREATE TABLE " + MEDICATION_TRACKER_TABLE + "("
+                + DOSE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + MED_ID + " INT,"
-                + LAST_TAKEN + " DATETIME, "
-                + NEXT_DOSE + " DATETIME,"
-                + "FOREIGN KEY (" + MED_ID + ") REFERENCES " + MEDICATION_TABLE + "(" + MED_ID + "),"
-                + "PRIMARY KEY (" + MED_ID + ")"
-                + ")";
-
-        queries[2] = "CREATE TABLE " + MEDICATION_HISTORY + "("
-                + MED_ID + " INT,"
-                + TAKEN + " BOOL,"
+                + DOSE_TIME + " DATETIME,"
+                + TAKEN + " BOOLEAN,"
                 + TIME_TAKEN + " DATETIME,"
-                + "FOREIGN KEY (" + MED_ID + ") REFERENCES " + MEDICATION_TABLE + "(" + MED_ID + "),"
-                + "PRIMARY KEY (" + MED_ID + ")"
+                + "FOREIGN KEY (" + MED_ID + ") REFERENCES " + MEDICATION_TABLE + "(" + MED_ID + ") ON DELETE CASCADE"
                 + ")";
 
-        queries[3] = "CREATE TABLE " + MEDICATION_STATS_TABLE + "("
-                + MED_ID + " INT,"
+        queries[2] = "CREATE TABLE " + MEDICATION_STATS_TABLE + "("
+                + MED_ID + " INT PRIMARY KEY ,"
                 + START_DATE + " DATETIME, "
                 + END_DATE + " DATETIME, "
                 + DOSES_TAKEN + " INT, "
                 + DOSES_MISSED + " INT,"
-                + "FOREIGN KEY (" + MED_ID + ") REFERENCES " + MEDICATION_TABLE + "(" + MED_ID + "),"
-                + "PRIMARY KEY (" + MED_ID + ")" +
-                ")";
+                + "FOREIGN KEY (" + MED_ID + ") REFERENCES " + MEDICATION_TABLE + "(" + MED_ID + ") ON DELETE CASCADE"
+                + ")";
 
-        queries[4] = "CREATE TABLE " + NOTES_TABLE + "("
-                + NOTE_ID + " INT,"
+        queries[3] = "CREATE TABLE " + NOTES_TABLE + "("
+                + NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + MED_ID + " INT, "
                 + NOTE + " TEXT, "
                 + ENTRY_TIME + " DATETIME,"
-                + "FOREIGN KEY (" + MED_ID + ") REFERENCES " + MEDICATION_TABLE + "(" + MED_ID +"),"
-                + "PRIMARY KEY (" + NOTE_ID + ")" +
-                ")";
+                + "FOREIGN KEY (" + MED_ID + ") REFERENCES " + MEDICATION_TABLE + "(" + MED_ID +") ON DELETE CASCADE"
+                + ")";
 
         for (int i = 0; i < NUM_TABLES; i++)
             sqLiteDatabase.execSQL(queries[i]);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db)
+    {
+        super.onOpen(db);
+        db.execSQL("PRAGMA foreign_keys = ON");
     }
 
     @Override
@@ -120,6 +115,34 @@ public class DBHelper extends SQLiteOpenHelper
         }
 
         onCreate(sqLiteDatabase);
+    }
+
+    public boolean addMedication(String medName, String patientName, String dosage, String units, ArrayList<String> times)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db1 = this.getReadableDatabase();
+        ContentValues medTableValues = new ContentValues();
+        ContentValues medTrackerValues = new ContentValues();
+
+        medTableValues.put(MED_NAME, medName);
+        medTableValues.put(PATIENT_NAME, patientName);
+        medTableValues.put(MED_DOSAGE, dosage);
+        medTableValues.put(MED_UNITS, units);
+
+        long rowid = db.insert(MEDICATION_TABLE, null, medTableValues);
+
+        if (rowid == -1)
+            return false;
+
+        medTrackerValues.put(MED_ID, rowid);
+        medTrackerValues.put(DOSE_TIME, 0);
+        medTrackerValues.put(TAKEN, 0);
+        medTrackerValues.put(TIME_TAKEN, 0);
+
+        if (db.insert(MEDICATION_TRACKER_TABLE, null, medTrackerValues) == -1)
+            return false;
+
+        return true;
     }
 
 
