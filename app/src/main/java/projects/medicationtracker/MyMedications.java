fc143e7be8a1bc37dd4100f9b22082416a6ab39c
AdamG95 <45023561+AdamG95@users.noreply.github.com>
2021-05-11 11:39:05 -0500

    Updates to MyMedications

--- app/src/main/java/projects/medicationtracker/MyMedications.java
+++ app/src/main/java/projects/medicationtracker/MyMedications.java
@@ -2,12 +2,17 @@ package projects.medicationtracker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -42,10 +47,28 @@ public class MyMedications extends AppCompatActivity
             }
             else
             {
+                if (patientNames.contains("ME!"))
+                        patientNames.set(patientNames.indexOf("ME!"), "You");
+
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, patientNames);
                 nameSpinner.setAdapter(adapter);
 
                 nameSpinner.setVisibility(View.VISIBLE);
+
+                if (patientNames.contains("You"))
+                        nameSpinner.setSelection(adapter.getPosition("You"));
+
+                nameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+                {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+                    {
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {}
+                });
+
             }
         }
     }
