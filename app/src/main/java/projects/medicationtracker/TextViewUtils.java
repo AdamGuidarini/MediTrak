a6b1db399ea01f2ed2cc267d2f76c5548c72afe5
AdamG95 <45023561+AdamG95@users.noreply.github.com>
2021-05-12 14:54:33 -0500

    Documentation

--- app/src/main/java/projects/medicationtracker/TextViewUtils.java
+++ app/src/main/java/projects/medicationtracker/TextViewUtils.java
@@ -5,6 +5,13 @@ import android.widget.TextView;
 
 public class TextViewUtils
 {
+    /**
+     * Sets predetermined parameters for a given TextView
+     * enabling a consistent style across the app
+     * @param textView The TextView to edit
+     * @param text Text to display
+     * @param parent The view to will hold the TextView
+     **************************************************************************/
     public static void setTextViewParams(TextView textView, String text, LinearLayout parent)
     {
         textView.setText(text);
@@ -15,6 +22,10 @@ public class TextViewUtils
             parent.addView(textView);
     }
 
+    /**
+     * Sets predetermined padding to TextView
+     * @param textView The TextView whose padding to set
+     **************************************************************************/
     public static void setTextViewFontAndPadding(TextView textView)
     {
         textView.setPadding(20, 24, 0, 24);
