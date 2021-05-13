6942ada41d8ca77b62ee2fde44012ac36d8bd019
AdamG95 <45023561+AdamG95@users.noreply.github.com>
2021-05-13 16:41:31 -0500

    Notes.java changed to Note.java

---
+++ app/src/main/java/projects/medicationtracker/Note.java
@@ -0,0 +1,46 @@
+package projects.medicationtracker;
+
+import java.time.LocalDateTime;
+
+public class Note
+{
+    private final long noteId;
+    private final long medId;
+    private String note;
+    private final LocalDateTime noteTime;
+
+    public Note(long note_Id, long med_Id, String nt, LocalDateTime time)
+    {
+        noteId = note_Id;
+        medId = med_Id;
+        note = nt;
+        noteTime = time;
+    }
+
+    // Getters and setters
+    public long getNoteId()
+    {
+        return noteId;
+    }
+
+    public long getMedId()
+    {
+        return medId;
+    }
+
+    public String getNote()
+    {
+        return note;
+    }
+
+    public void setNote(String note)
+    {
+        this.note = note;
+    }
+
+    public LocalDateTime getNoteTime()
+    {
+        return noteTime;
+    }
+
+}
