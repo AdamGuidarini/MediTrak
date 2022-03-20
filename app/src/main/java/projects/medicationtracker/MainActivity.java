10a1d9a8801d32f06cc174c7ee6a1efbaa9b15a5
Adam Guidarini <45023561+AdamG95@users.noreply.github.com>
2022-03-20 13:35:28 -0500

    Notification intents generate on boot, some CardCreator methods moved to MainActivity

--- app/src/main/java/projects/medicationtracker/MainActivity.java
+++ app/src/main/java/projects/medicationtracker/MainActivity.java
@@ -1,19 +1,28 @@
 package projects.medicationtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.util.Pair;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -25,7 +34,7 @@ public class MainActivity extends AppCompatActivity
      * Runs at start of activity, builds MainActivity
      *
      * @param savedInstanceState Stored instance of activity
-     **************************************************************************/
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -35,6 +44,7 @@ public class MainActivity extends AppCompatActivity
         Objects.requireNonNull(getSupportActionBar()).setTitle("Medication Schedule");
 
         NotificationHelper.createNotificationChannel(this);
+        prepareNotifications();
 
         createMainActivityViews();
     }
@@ -43,7 +53,7 @@ public class MainActivity extends AppCompatActivity
      * Creates option menu
      *
      * @param menu Menu containing selections for user
-     **************************************************************************/
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -54,7 +64,7 @@ public class MainActivity extends AppCompatActivity
     /**
      * Instructions for restarting activity once returned to from other activity
      * Clears all elements and reprints new ones.
-     **************************************************************************/
+     */
     @Override
     protected void onRestart()
     {
@@ -66,7 +76,7 @@ public class MainActivity extends AppCompatActivity
     *  Launches MyMedications.java when "My Medications" option is selected
     *
     *  @param item the "My Medications" menu option
-    **************************************************************************/
+    */
     public void onMyMedicationsClick(MenuItem item)
     {
         Intent intent = new Intent(this, MyMedications.class);
@@ -77,7 +87,7 @@ public class MainActivity extends AppCompatActivity
      *  Launches AddMedication.java when "Add Medication" option is selected
      *
      *  @param item The "Add Medication" option
-     **************************************************************************/
+     */
     public void onAddMedicationClick(MenuItem item)
     {
         Intent intent = new Intent(this, AddMedication.class);
@@ -88,13 +98,16 @@ public class MainActivity extends AppCompatActivity
      *  Launches Settings.java when "Settings" option is selected
      *
      *  @param item The "Settings" menu option
-     **************************************************************************/
+     */
     public void onSettingsClick(MenuItem item)
     {
         Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
     }
 
+    /**
+     * Creates CardViews for MainActivity
+     */
     public void createMainActivityViews()
     {
         LinearLayout scheduleLayout = findViewById(R.id.scheduleLayout);
@@ -119,7 +132,7 @@ public class MainActivity extends AppCompatActivity
         {
             patientNames.setVisibility(View.GONE);
 
-            CardCreator.createMedicationSchedule(medications, names.get(0), db, scheduleLayout);
+            createMedicationSchedule(medications, names.get(0), db, scheduleLayout);
         }
         else
         {
@@ -128,7 +141,11 @@ public class MainActivity extends AppCompatActivity
             if (names.contains("ME!"))
                 names.set(names.indexOf("ME!"), "You");
 
-            ArrayAdapter<String> patientAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, names);
+            ArrayAdapter<String> patientAdapter = new ArrayAdapter<>(
+                    this,
+                    android.R.layout.simple_dropdown_item_1line,
+                    names
+            );
             patientNames.setAdapter(patientAdapter);
 
             // Select "You" by default
@@ -153,12 +170,227 @@ public class MainActivity extends AppCompatActivity
                     if (name.equals("You"))
                         name = "ME!";
 
-                    CardCreator.createMedicationSchedule(medications, name, db, scheduleLayout);
+                    createMedicationSchedule(medications, name, db, scheduleLayout);
                 }
 
                 @Override
                 public void onNothingSelected(AdapterView<?> adapterView) {}
             });
+        }
+    }
+
+    /**
+     * Creates a schedule for the given patient's medications
+     *
+     * @param medications An ArrayList of Medications. Will be searched for
+     *                    Medications where patientName equals name passed to method.
+     * @param name The name of the patient whose Medications should be displayed
+     * @param db The database from which to pull data
+     * @param scheduleLayout The LinearLayout into which the cards will be placed
+     */
+    public static void createMedicationSchedule(ArrayList<Medication> medications, String name, DBHelper db, LinearLayout scheduleLayout)
+    {
+        ArrayList<Medication> medicationsForThisPatient = new ArrayList<>();
+
+        for (int i = 0; i < medications.size(); i++)
+        {
+            if (medications.get(i).getPatientName().equals(name))
+                medicationsForThisPatient.add(medications.get(i));
+        }
+
+        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        for (int ii = 0; ii < 7; ii++)
+            createDayOfWeekCards(days[ii], ii, medicationsForThisPatient, scheduleLayout, db, scheduleLayout.getContext());
+    }
+
+    /**
+     * Creates a CardView for each day of the week containing information
+     * on the medications to be taken that day
+     *
+     * @param dayOfWeek The day of the week represented by the CardView.
+     * @param day The number representing the day of the week
+     *            - Sunday = 0
+     *            - Monday = 1
+     *            - Tuesday = 2
+     *            - Wednesday = 3
+     *            - Thursday = 4
+     *            - Friday = 5
+     *            - Saturday = 6
+     * @param medications The list of medications to be taken on the given day
+     * @param layout The LinearLayout in which to place the CardView
+     */
+    public static void createDayOfWeekCards (String dayOfWeek, int day, ArrayList<Medication> medications, LinearLayout layout, DBHelper db, Context context)
+    {
+        CardView thisDayCard = new CardView(context);
+        TextView dayLabel = new TextView(context);
+        LinearLayout ll = new LinearLayout(context);
+
+        LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        ll.setLayoutParams(llParams);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        CardCreator.setCardParams(thisDayCard);
+
+        LocalDate thisSunday = TimeFormatting.whenIsSunday();
+
+        // Add day to top of card
+        TextViewUtils.setTextViewFontAndPadding(dayLabel);
+
+        String dayLabelString = dayOfWeek + " " + TimeFormatting.localDateToString(thisSunday.plusDays(day));
+
+        dayLabel.setText(dayLabelString);
+        ll.addView(dayLabel);
+
+        // Add medications
+        thisDayCard.addView(ll);
+
+        for (Medication medication : medications)
+        {
+            for (LocalDateTime time : medication.getTimes())
+            {
+                if (time.toLocalDate().isEqual(thisSunday.plusDays(day)))
+                {
+                    CheckBox thisMedication = new CheckBox(ll.getContext());
+                    long medId = medication.getMedId();
+                    Pair<Long, LocalDateTime> tag;
+
+                    // Set Checkbox label
+                    String medName = medication.getMedName();
+                    String dosage = medication.getMedDosage() + " " + medication.getMedDosageUnits();
+                    String dosageTime = TimeFormatting.formatTimeForUser(time.getHour(), time.getMinute());
+
+                    String thisMedicationLabel = medName + " - " + dosage + " - " + dosageTime;
+                    thisMedication.setText(thisMedicationLabel);
+
+                    // Check database for this dosage, if not add it
+                    // if it is, get the DoseId
+                    long rowid = 0;
+
+                    if (!db.isInMedicationTracker(medication, time))
+                    {
+                        LocalDateTime startDate = medication.getStartDate();
+                        if (!time.isBefore(startDate))
+                        {
+                            rowid = db.addToMedicationTracker(medication, time);
+
+                            if (rowid == -1)
+                                Toast.makeText(context, "An error occurred when attempting to write data to database", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else
+                    {
+                        rowid = db.getDoseId(medId, TimeFormatting.localDateTimeToString(time));
+                    }
+
+                    tag = Pair.create(rowid, time);
+
+                    if (rowid > 0)
+                    {
+                        thisMedication.setTag(tag);
+
+                        if (db.getTaken(rowid))
+                            thisMedication.setChecked(true);
+
+                        thisMedication.setOnCheckedChangeListener((compoundButton, b) ->
+                        {
+                            Pair<Long, LocalDateTime> tvTag = (Pair<Long, LocalDateTime>) thisMedication.getTag();
+                            final Long doseId = tvTag.first;
+                            int timeBeforeDose = db.getTimeBeforeDose();
+
+                            if (LocalDateTime.now().isBefore(time.minusHours(timeBeforeDose)) && timeBeforeDose != -1)
+                            {
+                                thisMedication.setChecked(false);
+                                Toast.makeText(context,
+                                        "Cannot take medications more than "
+                                                + timeBeforeDose + " hours in advance",
+                                        Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+
+                            String now = TimeFormatting.localDateTimeToString(LocalDateTime.now());
+                            db.updateDoseStatus(doseId, now, thisMedication.isChecked());
+                        });
+
+                        ll.addView(thisMedication);
+                    }
+                }
+            }
+        }
+
+        if (ll.getChildCount() == 1)
+        {
+            TextView textView = new TextView(thisDayCard.getContext());
+            String noMed = "No medications for " + dayOfWeek;
+
+            TextViewUtils.setTextViewParams(textView, noMed, ll);
+        }
+        else
+            sortMedicationCheckBoxes(ll);
+
+        layout.addView(thisDayCard);
+    }
+
+    /**
+     * Sorts CheckBoxes in medication schedule.
+     * @param parentLayout Layout containing CheckBoxes to sort.
+     */
+    public static void sortMedicationCheckBoxes(LinearLayout parentLayout)
+    {
+        int count = parentLayout.getChildCount();
+        short firstCheckboxIndex = 1;
+
+        for (int i = firstCheckboxIndex; i < count; i++)
+        {
+            for (int j = firstCheckboxIndex + 1; j < (count - i); j++)
+            {
+                CheckBox child1 = (CheckBox) parentLayout.getChildAt(j - 1);
+                CheckBox child2 = (CheckBox) parentLayout.getChildAt(j);
+
+                Pair<Long, LocalDateTime> child1Pair = (Pair<Long, LocalDateTime>) child1.getTag();
+                Pair<Long, LocalDateTime> child2Pair = (Pair<Long, LocalDateTime>) child2.getTag();
+
+                LocalDateTime child1Time = child1Pair.second;
+                LocalDateTime child2Time = child2Pair.second;
+
+                if (child1Time != null && child1Time.isAfter(child2Time))
+                {
+                    CheckBox temp = new CheckBox(parentLayout.getContext());
+                    temp.setText(child1.getText());
+                    temp.setTag(child1.getTag());
+
+                    child1.setText(child2.getText());
+                    child1.setTag(child2.getTag());
+
+                    child2.setText(temp.getText());
+                    child2.setTag(temp.getTag());
+                }
+            }
+        }
+    }
+
+    /**
+     * Prepares pending intents for notifications, useful if app is force stopped
+     */
+    private void prepareNotifications()
+    {
+        ArrayList<Medication> medications = db.getMedications();
+
+        for (Medication medication : medications)
+        {
+            LocalTime[] times = db.getMedicationTimes(medication.getMedId());
+            long[] timeIds = db.getMedicationTimeIds(medication);
+
+            for (int i = 0; i < times.length; i++)
+            {
+                NotificationHelper.scheduleNotification(
+                        this,
+                        medication,
+                        LocalDateTime.of(medication.getStartDate().toLocalDate(), times[i]),
+                        times.length > 1 ? timeIds[i] * -1 : medication.getMedId()
+                );
+            }
         }
     }
 }
