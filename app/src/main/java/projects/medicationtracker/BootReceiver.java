2862577749d6c82520b1a9886ef7c3e0cc2c29a6
Adam <adamguidarini@gmail.com>
2022-03-19 22:48:06 -0500

    Notifications work after reboot

--- app/src/main/java/projects/medicationtracker/BootReceiver.java
+++ app/src/main/java/projects/medicationtracker/BootReceiver.java
@@ -4,12 +4,41 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class BootReceiver extends BroadcastReceiver
 {
-
     @Override
     public void onReceive(Context context, Intent intent)
     {
+        final DBHelper db = new DBHelper(context);
+        ArrayList<Medication> medications = db.getMedications();
 
+        for (final Medication medication : medications)
+        {
+            prepareNotification(context, db, medication);
+        }
+    }
+
+    private void prepareNotification(Context context, DBHelper db, Medication medication)
+    {
+        LocalTime[] times = db.getMedicationTimes(medication.getMedId());
+        LocalDate startDate = medication.getStartDate().toLocalDate();
+        long[] timeIds = db.getMedicationTimeIds(medication);
+
+        for (int i = 0; i < times.length; i++)
+        {
+            long notificationId = times.length > 1 ? timeIds[i] * -1 : medication.getMedId();
+
+            NotificationHelper.scheduleNotification(
+                    context,
+                    medication,
+                    LocalDateTime.of(startDate, times[i]),
+                    notificationId
+            );
+        }
     }
 }
