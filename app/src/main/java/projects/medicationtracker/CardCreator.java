39d9351b24d974c47753a3d0e916336b3636b870
Adam Guidarini <45023561+AdamG95@users.noreply.github.com>
2022-09-14 23:12:34 -0500

    createNoteCard method moved

--- app/src/main/java/projects/medicationtracker/CardCreator.java
+++ app/src/main/java/projects/medicationtracker/CardCreator.java
@@ -1,46 +1,12 @@
 package projects.medicationtracker;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 
-import java.time.LocalTime;
-
 public class CardCreator
 {
-    /**
-     * Creates a CardView with a note in it
-     * @param note The Note in the CardView
-     * @param baseLayout The LinearLayout the holds the CardView
-     **************************************************************************/
-    public static void createNoteCard(Note note, LinearLayout baseLayout)
-    {
-        Context context = baseLayout.getContext();
-        CardView noteCard = new CardView(context);
-        LinearLayout cardLayout = new LinearLayout(context);
-
-        cardLayout.setOrientation(LinearLayout.VERTICAL);
-        setCardParams(noteCard);
-
-        baseLayout.addView(noteCard);
-        noteCard.addView(cardLayout);
-
-        TextView noteText = new TextView(context);
-        TextViewUtils.setTextViewParams(noteText, "\"" + note.getNote() + "\"", cardLayout);
-
-        TextView noteDate = new TextView(context);
-        String noteDateLabel = "Date: " + TimeFormatting.localDateToString(note.getNoteTime().toLocalDate())
-                + " at: " + TimeFormatting.localTimeToString(note.getNoteTime().toLocalTime());
-        TextViewUtils.setTextViewParams(noteDate, noteDateLabel, cardLayout);
-
-        noteText.setTag(note);
-    }
-
     /**
      * Sets a CardView's parameters to a set standard
      * @param cardView The CardView whose parameters will be set
