0ace4f74fe4f8a08b5756ff666a26cde9eb5b5bb
Adam Guidarini <45023561+AdamG95@users.noreply.github.com>
2022-09-28 21:12:22 -0500

    Started edit/add fragment

---
+++ app/src/main/java/projects/medicationtracker/Fragments/AddEditFormFragment.java
@@ -0,0 +1,61 @@
+package projects.medicationtracker.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import projects.medicationtracker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddEditFormFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddEditFormFragment extends Fragment
+{
+    final static String MED_ID = "medId";
+
+    private long medId = -1;
+
+    public AddEditFormFragment() { }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param medId ID if medication if editing, -1 if adding
+     * @return A new instance of fragment AddEditFormFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddEditFormFragment newInstance(long medId)
+    {
+        AddEditFormFragment fragment = new AddEditFormFragment();
+        Bundle args = new Bundle();
+        args.putLong(MED_ID, medId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null)
+        {
+            medId = getArguments().getLong(MED_ID, -1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_edit_form, container, false);
+    }
+}
