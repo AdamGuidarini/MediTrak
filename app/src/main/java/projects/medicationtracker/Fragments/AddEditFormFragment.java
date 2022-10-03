5d1760f3e92446ec739d2f83113125c2f2ad0854
Adam Guidarini <45023561+AdamG95@users.noreply.github.com>
2022-10-02 22:12:51 -0500

    Fragment is more interactive

--- app/src/main/java/projects/medicationtracker/Fragments/AddEditFormFragment.java
+++ app/src/main/java/projects/medicationtracker/Fragments/AddEditFormFragment.java
@@ -7,8 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+
+import projects.medicationtracker.Helpers.DBHelper;
 import projects.medicationtracker.R;
+import projects.medicationtracker.SimpleClasses.Medication;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,9 +29,28 @@ import projects.medicationtracker.R;
  */
 public class AddEditFormFragment extends Fragment
 {
-    final static String MED_ID = "medId";
+    final public static String MED_ID = "medId";
+    private DBHelper db;
 
     private long medId = -1;
+    private Medication medication;
+    private View rootView;
+
+    private RadioGroup patientGroup;
+    private RadioButton meButton;
+    private RadioButton otherButton;
+    private TextInputLayout patientNameInputLayout;
+    private EditText patientNameInput;
+
+    private EditText medNameInput;
+    private SwitchMaterial aliasSwitch;
+    private TextInputLayout aliasInputLayout;
+    private EditText aliasInput;
+    private EditText dosageAmountInput;
+    private EditText dosageUnitsInput;
+
+    //TODO add all frequency inputs
+    private MaterialAutoCompleteTextView frequencyDropDown;
 
     public AddEditFormFragment() { }
 
@@ -54,7 +85,86 @@ public class AddEditFormFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_form, container, false);
+        rootView = inflater.inflate(R.layout.fragment_add_edit_form, container, false);
+        db = new DBHelper(rootView.getContext());
+
+        medication = medId != -1 ? db.getMedication(medId) : null;
+
+        buildViews();
+
+        return rootView;
+    }
+
+    private void buildViews()
+    {
+        setPatientCard();
+        setMedNameAndDosageCard();
+        setFrequencyCard();
+    }
+
+    private void setPatientCard()
+    {
+        patientGroup = rootView.findViewById(R.id.patientRadioGroup);
+        meButton = rootView.findViewById(R.id.patientIsMe);
+        otherButton = rootView.findViewById(R.id.patientIsNotMe);
+        patientNameInput = rootView.findViewById(R.id.patientNameInput);
+        patientNameInputLayout = rootView.findViewById(R.id.patientNameInputLayout);
+
+        if (medId == -1)
+        {
+            meButton.setChecked(true);
+        }
+        else if (medication.getPatientName().equals("ME!"))
+        {
+            meButton.setChecked(true);
+        }
+        else
+        {
+            otherButton.setChecked(true);
+            patientNameInputLayout.setVisibility(View.VISIBLE);
+            patientNameInput.setText(medication.getPatientName());
+        }
+
+        patientGroup.setOnCheckedChangeListener((radioGroup, i) ->
+        {
+            switch (radioGroup.findViewById(i).getId())
+            {
+                case R.id.patientIsMe:
+                    if (patientNameInputLayout.getVisibility() == View.VISIBLE)
+                        patientNameInputLayout.setVisibility(View.GONE);
+                    break;
+                case R.id.patientIsNotMe:
+                    patientNameInputLayout.setVisibility(View.VISIBLE);
+                    break;
+            }
+        });
+    }
+
+    private void setMedNameAndDosageCard()
+    {
+        medNameInput = rootView.findViewById(R.id.medicationName);
+        aliasSwitch = rootView.findViewById(R.id.aliasSwitch);
+        aliasInput = rootView.findViewById(R.id.enterAlias);
+        aliasInputLayout = rootView.findViewById(R.id.aliasInputLayout);
+        dosageAmountInput = rootView.findViewById(R.id.dosageAmount);
+        dosageUnitsInput = rootView.findViewById(R.id.dosageUnits);
+
+        aliasSwitch.setChecked(medId != -1 && !medication.getAlias().isEmpty());
+
+        aliasSwitch.setOnCheckedChangeListener((compoundButton, b) ->
+        {
+            if (aliasInputLayout.getVisibility() == View.GONE)
+                aliasInputLayout.setVisibility(View.VISIBLE);
+            else
+            {
+                aliasInputLayout.setVisibility(View.GONE);
+                aliasInput.setText("");
+            }
+        });
+    }
+
+    private void setFrequencyCard()
+    {
+
     }
 }
