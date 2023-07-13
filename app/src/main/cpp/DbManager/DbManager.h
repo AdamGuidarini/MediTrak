2859b41cc1637e3745deba293174ae3170353f77
Adam Guidarini <45023561+AdamGuidarini@users.noreply.github.com>
2023-07-13 18:54:03 -0500

    Additional documentation

--- app/src/main/cpp/DbManager/DbManager.h
+++ app/src/main/cpp/DbManager/DbManager.h
@@ -21,18 +21,18 @@ using namespace std;
 class DbManager {
 private:
     /**
-     * Path to the database file
+     * Path to the database file.
      */
     string database_name;
     /**
-     * Database reference
+     * Database reference.
      */
     sqlite3* db{};
 
 public:
     /**
      * Constructor for DbManager class, automatically opens database.
-     * @param fileDescriptor Name of file containing database
+     * @param fileDescriptor Name of file containing database.
      */
     DbManager(string fileDescriptor);
 
@@ -42,24 +42,24 @@ public:
     ~DbManager();
 
     /**
-     * Opens database
+     * Opens database.
      */
     void openDb();
 
     /**
-     * Closes database
+     * Closes database.
      */
     void closeDb();
 
     /**
-     * Gets all tables in database
-     * @return A pointer to a string to hold table names
+     * Gets all tables in database.
+     * @return A pointer to a string to hold table names.
      */
     vector<string> getTables();
 
     /**
-     * Retrieves all values from a database table
-     * @param table The table from which to retrieve results
+     * Retrieves all values from a database table.
+     * @param table The table from which to retrieve results.
      * @return A vector containing all values in "table".
      *      Each element of the vector is a map whose key is the column name
      *      and value is the column value.
@@ -67,23 +67,25 @@ public:
     vector<map<string, string>> readAllValuesInTable(const string& table);
 
     /**
-     * Retrieves all data from all tables in the database
+     * Retrieves all data from all tables in the database.
      * @return A map to contain all data in the database.
      *      Each key should be a table name.
      *      Each value should be a pointer to vector a maps whose keys are column names
-     *      and values are column values
+     *      and values are column values.
      */
     map<string, vector<map<string, string>>> getAllRowFromAllTables();
 
     /**
-     * Exports all data stored in DB to provided location as a JSON file
-     * @param exportDirectory Location for file storing database backup
+     * Exports all data stored in DB to provided location as a JSON file.
+     * @param exportDirectory Location for file storing database backup.
      */
     void exportData(const string& exportDirectory);
 
     /**
-     * Imports data from JSON file and writes it to database
-     * @param importFilePath
+     * Imports data from JSON file and writes it to database,
+     *  throws an error if attempting to write to a table or
+     *  column that does not exist in the provided database/table.
+     * @param importFilePath Path to JSON file storing data to import.
      */
     void importData(const string& importFilePath);
 };
