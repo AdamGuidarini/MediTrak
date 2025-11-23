e1b8ce0e628b759812cda075afc9b326b7f2bf11
Adam Guidarini <45023561+AdamGuidarini@users.noreply.github.com>
2025-11-22 23:04:03 -0700

    New screenshots

--- app/src/main/java/projects/medicationtracker/BaseActivity.java
+++ app/src/main/java/projects/medicationtracker/BaseActivity.java
@@ -1,18 +1,15 @@
 package projects.medicationtracker;
 
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.View;
+
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
-import android.content.res.Configuration;
-import android.view.ViewGroup;
+import androidx.activity.ComponentActivity;
 
 /**
  * A simple activity to apply the new edge to edge window insets.
@@ -24,15 +21,7 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        WindowInsetsControllerCompat windowInsetsController =
-                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
-
-        boolean isLightMode = (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK)
-                == Configuration.UI_MODE_NIGHT_NO;
-
-        windowInsetsController.setAppearanceLightStatusBars(isLightMode);
-        windowInsetsController.setAppearanceLightNavigationBars(isLightMode);
+        EdgeToEdge.enable(this);
 
         View contentView = findViewById(android.R.id.content);
         if (contentView != null) {
